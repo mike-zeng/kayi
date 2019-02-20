a615c8865ceca87b5b119289eee33b1306df2e9a
zeng <807920489@qq.com>
2019-02-20 20:58:43 +0800

    Initial commit

---
+++ src/main/java/connector/Connector.java
@@ -0,0 +1,7 @@
+package connector;
+
+/**
+ * 连接器接口，凡是实现了该接口的连接器都可以被使用
+ */
+public interface Connector {
+}
---
+++ src/main/java/connector/http/HttpParser.java
@@ -0,0 +1,8 @@
+package connector.http;
+
+/**
+ * http解析器，将http请求解析成HttpRequest实例
+ * @author zeng
+ */
+public class HttpParser {
+}
